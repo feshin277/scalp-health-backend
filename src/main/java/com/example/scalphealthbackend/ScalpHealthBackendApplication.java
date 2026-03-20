52a0188f86a35d29752abeb8e4985ecad6a048bb
feshin277 <fuyuxun294@gmail.com>
2026-03-20 19:50:13 +0800

    Create Spring Boot main application class

---
+++ src/main/java/com/example/scalphealthbackend/ScalpHealthBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.scalphealthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalpHealthBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScalpHealthBackendApplication.class, args);
+    }
+}
